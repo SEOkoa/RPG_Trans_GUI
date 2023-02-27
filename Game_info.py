9d00e668eba4b1258d2edcd2717fe5223a83190d
seo.koa <kaze11koa@gmail.com>
2023-02-28 04:10:15 +0900

    sorted file structure applied drag&drop

--- Game_info.py
+++ Game_info.py
@@ -6,24 +6,3 @@ from PySide2.QtGui import QDragEnterEvent, QDropEvent
 from PySide2.QtWidgets import QLabel, QSizePolicy
 # ---------------본문--------------- #
 
-class LocationDragDrop(QLabel):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.setAcceptDrops(True)
-        self.setText("Drop folder here")
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.setAlignment(Qt.AlignCenter)
-
-    def dragEnterEvent(self, event: QDragEnterEvent):
-        if event.mimeData().hasUrls() and len(event.mimeData().urls()) == 1:
-            url = event.mimeData().urls()[0]
-            if url.isLocalFile() and Path(url.toLocalFile()).is_dir():
-                event.acceptProposedAction()
-
-    def dropEvent(self, event: QDropEvent):
-        url = event.mimeData().urls()[0]
-        folder_path = Path(url.toLocalFile())
-        self.setText(f"Folder path: {folder_path}")
-
-    def Test_Event(self):
-        self.Folder_Name_label.setText('hello')
--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 
 ### special thanks (REPOSITORY)
 # https://github.com/pyside
+# https://github.com/5yutan5/PyQtDarkTheme
 # https://github.com/aeerso/rgss3a-decrypter     //rgss3a 파일 암호화 해제 관련
+# https://gitlab.com/rgss/rgsstool/-/blob/master/rgsstool.py // 여기도
 # https://github.com/HelloKS/ezTransWeb          //eztrans 번역 관련
-# https://gitlab.com/rgss/rgsstool/-/blob/master/rgsstool.py
+
 
 
 ### special thanks (HELP)
 # https://github.com/mongbro
-#=================================#
+
+#------------------import------------------#
 from PySide2.QtUiTools import *
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
@@ -19,21 +22,142 @@ from pathlib import Path
 from Game_info import *
 
 import sys, os
-#-------------본문------------------#
+import qdarktheme
+#------------------본문------------------#
 
 
-if __name__ == '__main__':
-    app = QApplication([])
+class MainUI(QObject):
+    def __init__(self, ui_file):
+        super().__init__()
+
+        # Load the UI file
+        loader = QUiLoader()
+        ui_file = QFile(ui_file)
+        ui_file.open(QFile.ReadOnly)
+        self.window = loader.load(ui_file)
+        ui_file.close()
+
+        # UI 파일의 위젯 정의
+        # ex) self.여기서의 위젯명 ... "ui파일에서의 위젯명"
+
+        #드래그 앤 드롭 아이콘
+        self.drag_drop_label = self.window.findChild(QLabel, "Location_Drag_Drop")
+        self.folder_info_widget = self.window.findChild(QWidget, "Folder_info_widget")
+
+        #폴더 정보 라벨
+        self.folder_name_label = self.window.findChild(QLabel, "Folder_Name_label")
+        self.is_encrypt_label = self.window.findChild(QLabel, "Is_Encrypt_label")
+        self.rpg_ver_label = self.window.findChild(QLabel, "RPG_Ver_label")
+
+        # 폴더 탐색 관련 위젯
+        self.file_control_layout = self.window.findChild(QHBoxLayout,"FileControl_layout")
+        self.folder_structure_tree = self.window.findChild(QTreeWidget,"FolderStructure_Tree")
+        self.preview_table = self.window.findChild(QTableWidget,"PreView_Table")
+
+        # Install an event filter on the drag_drop_label label to handle mouse press events
+        self.drag_drop_label.setPixmap('UI/download_icon.svg')
+        self.drag_drop_label.installEventFilter(self)
+        
+
+        # Show the main window
+        self.window.show()
+
+    @staticmethod
+    def version_check(self, folder_path):
+        if Path(folder_path).joinpath('game.ini').exists():
+            engine = 'RPG XP/VX'
+            self.rpg_ver_label.setText(engine)
+
+    @staticmethod
+    def encryption_check(self, folder_path):
+        if Path(folder_path).joinpath('Game.rgss3a').exists():
+            encryption = 'rgss3a로 암호화됨'
+            self.is_encrypt_label.setText(encryption)
+            
+     # 드래그 앤 드롭 라벨 아이콘을 실행파일 아이콘으로 바꿈        
+    @staticmethod
+    def get_launcher_icon(self, folder_path):
+       
+        launcher_path = folder_path + '/Game.exe'
+        icon_provider = QFileIconProvider()
+        file_icon = icon_provider.icon(launcher_path)
+        self.drag_drop_label.setPixmap(file_icon.pixmap(512, 512))  # set a 32x32 pixmap
+
+    # 폴더가 선택되었을 때, 폴더를 검사하는 함수
+    @staticmethod
+    def analyze_folder(self, folder_path):
+
+        #상단에 게임 기본 정보 분석
+        self.folder_name_label.setText(folder_path) #폴더 경로는 여기서 표시
+        self.folder_structure_tree.clear() # 새 내용 표시를 위해 Tree위젯 초기화
+
+        # root 폴더를 tree widget에 추가
+        root_item = QTreeWidgetItem(self.folder_structure_tree)
+        root_item.setText(0, os.path.basename(folder_path))
+        root_item.setToolTip(0, folder_path)
+
+        #하위 폴더들을 보여주기 위해 함수 호출
+        self.populate_tree(folder_path, root_item)
+        self.version_check(self, folder_path)
+        self.encryption_check(self, folder_path)
+        self.get_launcher_icon(self, folder_path)
+      
+ 
+
+    # 클릭해서 직접 폴더 추가
+    def eventFilter(self, source, event):
+        # drag_drop 아이콘을 직접 누르면 직접 폴더를 추가
+        if source == self.drag_drop_label and event.type() == QEvent.MouseButtonPress:
+            # Open a file dialog to select a folder
+            folder_path = QFileDialog.getExistingDirectory(None, "Select Folder")
+
+            # 폴더가 선택되었을 경우,
+            if folder_path:
+                self.analyze_folder(self, folder_path)
+            # Return True to indicate that the event has been handled
+            return True
+
+        # 드래그 앤 드롭 구현 부분
+        elif source == self.drag_drop_label and event.type() == QEvent.DragEnter:
+
+            # Check if the mime data of the event contains a local file URL that represents a directory
+            if event.mimeData().hasUrls() and len(event.mimeData().urls()) == 1:
+                url = event.mimeData().urls()[0]
+                if url.isLocalFile() and os.path.isdir(url.toLocalFile()):
+                    event.acceptProposedAction()
+
+        # 아이콘에 폴더가 드롭 되었을 때
+        elif source == self.drag_drop_label and event.type() == QEvent.Drop:
+            # Get the path of the dropped folder and display it in the Folder_Name_label
+            url = event.mimeData().urls()[0]
+            folder_path = url.toLocalFile()
+            self.analyze_folder(self, folder_path)
+            
+
+        # Otherwise, call the base class eventFilter to handle the event normally
+        return super().eventFilter(source, event)
+
+    #TreeWidget에 하위폴더를 표현하는 함수
+    def populate_tree(self, folder_path, parent_item):
+        # Iterate over the contents of the folder
+        for file_name in os.listdir(folder_path):
+            # Construct the full path of the file or folder
+            full_path = os.path.join(folder_path, file_name)
+
+            # Create a tree widget item for the file or folder
+            item = QTreeWidgetItem(parent_item)
+            item.setText(0, file_name)
+            item.setToolTip(0, full_path)
+
+            # If the item is a folder, recursively add its contents to the tree widget
+            if os.path.isdir(full_path):
+                self.populate_tree(full_path, item)
+
     
-    # Load the UI file
-    loader = QUiLoader()
-    ui_file = QFile('UI/QT_UI.ui')
-    ui_file.open(QFile.ReadOnly)
-    window = loader.load(ui_file)
-    ui_file.close()
-
-
-    # Show the main window
-    window.show()
-    # Start the event loop
+#ui 파일 위치 지정
+if __name__ == "__main__":
+    app = QApplication([])
+    qdarktheme.setup_theme("dark")
+    main_ui = MainUI("UI/Main_UI.ui")
+    main_ui.window.show()
     app.exec_()
