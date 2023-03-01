7c8287aef9ca115222cd4518d0d844bcdb502e55
seo.koa <kaze11koa@gmail.com>
2023-03-02 02:23:42 +0900

    applied tabview and viewer functions

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ from PySide2.QtGui import *
 
 from pathlib import Path
 from bs4 import BeautifulSoup as bs
+from pandas_ods_reader import read_ods
+
 import requests
 import seaborn as sns
 import pandas as pd
@@ -28,6 +30,7 @@ import pandas as pd
 import sys, os
 import qdarktheme
 #------------------본문------------------#
+
 
 
 class MainUI(QObject):
@@ -60,10 +63,19 @@ class MainUI(QObject):
         self.file_control_layout = self.window.findChild(QHBoxLayout,"FileControl_layout")
         self.folder_structure_tree = self.window.findChild(QTreeWidget,"FolderStructure_Tree")
 
-        self.txtview_widget = self.window.findChild(QPlainTextEdit,"TXTView_Widget")
-        self.csvview_Widget = self.window.findChild(QTableWidget,"CSVView_Widget")
-        self.odsview_widget = self.window.findChild(QTableWidget,"ODSView_Widget")
+        # 탭 위젯 정의
+        self.tab_Widget = self.window.findChild(QTabWidget, "Tab_Widget")
 
+        self.txtview_Tab = self.window.findChild(QWidget, "TXTVIEW_Tab")
+        self.csvview_Tab = self.window.findChild(QWidget, "CSVVIEW_Tab")
+        self.odsview_Tab = self.window.findChild(QWidget, "ODSVIEW_Tab")
+        self.logview_Tab = self.window.findChild(QWidget, "LOGVIEW_Tab")
+
+        self.txtview_Widget = self.window.findChild(QTextEdit,"TXTView_Widget")
+        self.csvview_Widget = self.window.findChild(QTableWidget,"CSVView_Widget")
+        self.odsview_Widget = self.window.findChild(QTableWidget,"ODSView_Widget")
+        self.logview_Widget = self.window.findChild(QTableWidget,"LOGView_Widget")
+    
         # 폴더 탐색 관련 시그널 정의
         self.folder_structure_tree.itemSelectionChanged.connect(self.on_item_selected)
 
@@ -75,7 +87,6 @@ class MainUI(QObject):
         self.dummy_btn_2 = self.window.findChild(QPushButton, "Dummy_btn_2")
 
         
-       
         # 사이드바 버튼 초기화(비활성)
         self.decrypt_btn.setEnabled(False)
         self.trans_btn.setEnabled(False)
@@ -90,6 +101,8 @@ class MainUI(QObject):
 
         # Show the main window
         self.window.show()
+
+        
 
     #특정 파일이나 폴더가 있는지를 통해 쯔꾸르 버전을 확인하는 함수
     @staticmethod
@@ -161,6 +174,77 @@ class MainUI(QObject):
         self.encryption_check(self, folder_path)
         self.get_launcher_icon(self, folder_path)
       
+    @staticmethod
+    def previewer_sensor(path, item_extension, encoding):
+        if item_extension == '.csv':
+            df = pd.read_csv(path, encoding=encoding)
+            return df, encoding
+        elif item_extension == '.ods':
+            df = read_ods(path)
+            return df, encoding
+        else:
+            return df, encoding
+            
+    @staticmethod
+    def previewer_sensor_txt(path, item_extension, encoding):
+        with open(path, 'rb') as f:
+            text = f.read()
+            return text
+
+
+
+    @staticmethod
+    def previewer_switch(self, path, item_extension):
+        
+        # Find the correct encoding
+        encodings = ['utf-8', 'euc-kr', 'cp949' ]
+        for encoding in encodings:
+            try:
+                if item_extension == '.csv' or item_extension == '.ods':
+                    df, encoding = self.previewer_sensor(path, item_extension, encoding )
+
+                elif item_extension == '.txt':
+                    text = self.previewer_sensor_txt(path, item_extension, encoding)
+            
+                break
+            except UnicodeDecodeError:
+                continue
+
+        if item_extension == '.csv' or item_extension == '.ods':
+            column_amount = len(df.columns)
+            row_amount = len(df)
+            # csvviewer or odsviewer initial settings
+            if item_extension == '.csv':
+                view_widget = self.csvview_Widget
+                self.tab_Widget.setCurrentIndex(1)
+            else:
+                view_widget = self.odsview_Widget
+                self.tab_Widget.setCurrentIndex(2)
+
+
+            view_widget.clear()
+            view_widget.setRowCount(row_amount)
+            view_widget.setColumnCount(column_amount)
+
+            
+            for j in range(column_amount):
+                header_item = QTableWidgetItem(df.columns[j])
+                view_widget.setHorizontalHeaderItem(j, header_item)
+
+            for i in range(row_amount):
+                for j in range(column_amount):
+                    item = QTableWidgetItem(str(df.iloc[i, j]))
+                    view_widget.setItem(i, j, item)
+
+        else:
+                # Display the file contents in txtview_widget if the file extension is not csv or ods
+                self.txtview_Widget.setPlainText(text.decode(encoding))
+                self.tab_Widget.setCurrentIndex(0)
+
+                
+
+                
+
     # 특정 확장자를 가진 파일에만 체크박스 생성  
     def checkFileExtension(self, item: QTreeWidgetItem, column: int):
         # 허용되지 않는 확장자 목록
@@ -209,11 +293,13 @@ class MainUI(QObject):
 
     # 하위폴더 탐색 함수
     def populate_tree(self, folder_path, parent_item):
-    # Iterate over the contents of the folder
+    # filen_name = 파일명
+    
+
         for file_name in os.listdir(folder_path):
             # Construct the full path of the file or folder
             full_path = os.path.join(folder_path, file_name)
-
+            
             # Create a tree widget item for the file or folder
             item = QTreeWidgetItem(parent_item)
             item.setText(0, file_name)
@@ -225,11 +311,15 @@ class MainUI(QObject):
             else:
                 # If the item is a file, add a checkbox and disable it if the extension is not in the allowed list
                 extension = os.path.splitext(file_name)[1]
+
                 if extension not in ('.txt', '.csv', '.ods'):
                     item.setFlags(item.flags() & ~Qt.ItemIsUserCheckable)
                 else:
                     item.setFlags(item.flags() | Qt.ItemIsUserCheckable)
                     item.setCheckState(0, Qt.Unchecked)
+                #여기가 폴더 검사할 때 확장자 체크하고 체크박스 활성화 하는 부분인데
+                # 좀만 수정하면 여기서 필터링 on/off식으로 구현 가능할듯
+                # 지금 익스텐션은 이 안에서만 사용할 수 있는 변수임
 
     # 선택된 파일을 좌측 프리뷰 창으로 보여주는 함수
     def on_item_selected(self):
@@ -241,30 +331,13 @@ class MainUI(QObject):
         # get the path of the selected item
         item = selected_items[0]
         path = item.toolTip(0)
+        item_extension = os.path.splitext(path)[1]
+        #path는 D:// ... filename.csv식으로 저장됨
+        #full_path도 마찬가지임
 
-        # 맞는 인코딩 찾기
-        encodings = ['utf-8', 'euc-kr', 'cp949']
-        for encoding in encodings:
-            try:
-                with open(path, encoding=encoding) as f:
-                    content = f.read()
-                df = pd.read_csv(path, encoding=encoding)
-                break
-            except UnicodeDecodeError:
-                continue
+        self.previewer_switch(self, path, item_extension)
 
-        csv_column_amount = len(df.columns)
-        csv_row_amount = len(df)
-
-        # csvviewer가 파일을 읽을 때의 초기 설정
-        self.csvview_Widget.clear()
-        self.csvview_Widget.setRowCount(csv_row_amount)
-        self.csvview_Widget.setColumnCount(csv_column_amount)
-        for i in range(csv_row_amount):
-            for j in range(csv_column_amount):
-                item = QTableWidgetItem(str(df.iloc[i, j]))
-                self.csvview_Widget.setItem(i, j, item)
-            
+       
 
 # from Main_UI.ui, 메인 윈도우 호출
 if __name__ == "__main__":
